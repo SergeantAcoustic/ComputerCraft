b6dc249016d0917d08e383f4fb991c5f7a5ee493
Home Desktop <tbaugh15@cowesec.org>
2023-01-19 00:32:28 +0000

    removed automatic api adding code, both broken and useless

--- files/autorun/setPaths.lua
+++ files/autorun/setPaths.lua
@@ -7,28 +7,3 @@
 -- add internal help path to help path
 local helpPath = help.path()..":"..fs.combine(fs.getDir(fs.getDir(shell.getRunningProgram())),"/help")
 help.setPath(helpPath)
-
--- adds apis
-local function addAPIFolder(path)
-    local prevPath = shell.path()
-    shell.setPath(path)
-    
-    local APIList = fs.list(path)
-
-    for i=1, #APIList do
-        local API = APIList[i]
-        os.loadAPI(API)
-    end
-    
-    shell.setPath(prevPath)
-end
-
-local folders = fs.list("/")
-for i=1, #folders do
-    if fs.isDir(folders[i]) then
-        APIPath = fs.combine(folders[i], "/apis/")
-        if fs.exists(APIPath) and folders[i] ~= "rom" then
-            addAPIFolder(APIPath)
-        end
-    end
-end
