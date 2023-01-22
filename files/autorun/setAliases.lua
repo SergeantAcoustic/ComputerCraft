8799f673b8e3ad0c8bf27c43e1cb8a613d317985
Home Desktop <tbaugh15@cowesec.org>
2023-01-22 15:19:25 +0000

    setAlias: program path workaround by adding all programs as aliases

--- files/autorun/setAliases.lua
+++ files/autorun/setAliases.lua
@@ -1,7 +1,16 @@
 -- add custom aliases
 local aliases = {e="edit", cls="clear", man="help",
-    screen="monitor", rn="rename",
-    tree="/"..fs.combine(fs.getDir(fs.getDir(shell.getRunningProgram())), "/programs/tree.lua")}
+    screen="monitor", rn="rename"}
+
+-- program path bodge though aliases
+local programPath = fs.combine(fs.getDir(fs.getDir(shell.getRunningProgram())), "/programs/")
+local programList = fs.list(programPath)
+-- aliases[tree] = "/"..fs.combine(fs.getDir(fs.getDir(shell.getRunningProgram())), "/programs/tree.lua")
+for _, programName in ipairs(programList) do
+	if not (fs.isDir(fs.combine(programPath, programName))) then
+		aliases[string.sub(programName, 1, -4)] = fs.combine(programPath, programName)
+	end
+end
 
 for key, value in pairs(aliases) do
     shell.setAlias(key, value)
