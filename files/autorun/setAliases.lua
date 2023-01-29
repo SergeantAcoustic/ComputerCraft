bd0aee5c6c3164472373bc6d64d6cbdef9a2195d
Home Desktop <tbaugh15@cowesec.org>
2023-01-29 20:19:29 +0000

    setAlias: program path now relative to root

--- files/autorun/setAliases.lua
+++ files/autorun/setAliases.lua
@@ -8,7 +8,7 @@ local programList = fs.list(programPath)
 -- aliases[tree] = "/"..fs.combine(fs.getDir(fs.getDir(shell.getRunningProgram())), "/programs/tree.lua")
 for _, programName in ipairs(programList) do
 	if not (fs.isDir(fs.combine(programPath, programName))) then
-		aliases[string.sub(programName, 1, -5)] = fs.combine(programPath, programName)
+		aliases[string.sub(programName, 1, -5)] = "/"..fs.combine(programPath, programName)
 	end
 end
 
